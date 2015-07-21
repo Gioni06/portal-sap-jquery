231627eb8ff5b5aeced5567174b123fc11d7a235
Gioni06 <jd@oygo.com>
2015-07-21 11:40:07 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,74 @@
+/**
+ * Demo Portal with SAP integration
+ */
+
+// GLOBALS
+
+var $DATA;
+
+// Changes XML to JSON
+function xmlToJson(xml) {
+
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+        for(var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof(obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof(obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
+};
+
+$(document).ready(function(){
+
+    $('#emailLoginFormButton').on('click',function(e){
+        e.preventDefault();
+
+        $.ajax({
+            url: 'https://lab-demo.qsc.de/sap/opu/odata/sap/zomnichannel_srv/WifiSet',
+            xhrFields: {
+                withCredentials: true
+            },
+            headers: {
+
+                'Authorization':'Basic b21uaWNoYW5uZWw6U29tbWVyaW5IYW1idXJnMjAwMA==',
+                'X-CSRF-Token': 'Fetch'
+            },
+            accepts:"application/xml",
+            method: 'GET',
+            dataType: 'xml',
+            data: {},
+            success: function(xml){
+                $DATA = xml;
+            }
+        });
+
+    })
+
+}); // Dom ready end
